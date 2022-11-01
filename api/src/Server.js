8d71afa29c45bd789d6faa0d3aac475a3cc744e2
lakshmi <lakshmijayaram83@gmail.com>
2022-11-01 19:42:12 -0300

    challenge1 done

--- api/src/Server.js
+++ api/src/Server.js
@@ -1,2 +1,29 @@
 // Express.js Server Code goes here
 // ...
+let express = require("express");
+let app = express();
+
+let fileSystem = require("fs");
+
+console.log("Express On----");
+
+app.get("/", (request, response) => {
+    response.send("<b>Hello world :)</b>");
+});
+
+app.get("/poem/:langName", (request, response) => { 
+    let fileName = `poem${request.params.langName.toUpperCase()}.html`;
+    console.log('filname---', fileName);    
+    console.log(__dirname);
+    fileSystem.readFile(fileName, (error, data) => {        
+        if(data){
+            response.sendFile('/usr/app/api/'+fileName);
+                
+        }else{
+            response.send("Language is not supported");
+        }  
+    });    
+}); 
+
+
+app.listen(80, () => console.log("Express listening on port 80"));
